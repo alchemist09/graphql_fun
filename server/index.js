79808a236c4a4f6f28ba8d16c061ad5bf99a2d78
Luke Olali <mugapedia@gmail.com>
2020-02-20 11:02:15 +0300

    Adds the graphql schema to express middleware

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
+const schema = require('./schema/schema');
+
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-  
+  schema
 }))
 
 app.listen(4000, () => {
+  // console.log(gs);
   console.log("Listening for requests on port 4000");
 });
 
