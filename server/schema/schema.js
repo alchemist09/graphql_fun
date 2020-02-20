b85374138f571871f34dc9aa6a21a85c4fbf0c40
Luke Olali <mugapedia@gmail.com>
2020-02-20 10:56:16 +0300

    Exports the schema that defines the structure of the graphql API

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -18,8 +18,12 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        
+
       }
     }
   }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
