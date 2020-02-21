5068d5c17d0c779ce42349598fb577c474f153a3
Luke Olali <mugapedia@gmail.com>
2020-02-21 13:16:28 +0300

    - Changes the id property values to strings - Uses lodash to resolve queries made at the root of the graphql schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,12 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const books = [
-  { id: 1, name: "High Performance JavaScript", genre: "Scalability"},
-  { id: 2, name: "Angles and Demons", genre: "Science Fiction"},
-  { id: 3, name: "Code Complete", genre: "Software Engineering"}
+  { id: "1", name: "High Performance JavaScript", genre: "Scalability"},
+  { id: "2", name: "Angles and Demons", genre: "Science Fiction"},
+  { id: "3", name: "Code Complete", genre: "Software Engineering"}
 ]
 
 const BookType = new GraphQLObjectType({
@@ -24,7 +25,7 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-
+        return _.find(books, { id: args.id})
       }
     }
   }
