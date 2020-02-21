9a6bd136bfc01ccceda70db68037ede570b4f9ad
Luke Olali <mugapedia@gmail.com>
2020-02-21 16:12:41 +0300

    Creates a root entry point on the graphql schema that returns a list of all books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -72,6 +72,12 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         return _.find(authors, { id: args.id })
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books
+      }
     }
   }
 })
