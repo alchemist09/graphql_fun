f4f5dd12f2e8d1e9e4a3b72e68fa6cd2356de213
Luke Olali <mugapedia@gmail.com>
2020-02-21 13:07:09 +0300

    Creates dummy data for books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,12 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+const books = [
+  { id: 1, name: "High Performance JavaScript", genre: "Scalability"},
+  { id: 2, name: "Angles and Demons", genre: "Science Fiction"},
+  { id: 3, name: "Code Complete", genre: "Software Engineering"}
+]
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
