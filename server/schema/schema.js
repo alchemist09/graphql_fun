adf11764defb96c43de80fb0253d6163dff5f534
Luke Olali <mugapedia@gmail.com>
2020-02-25 16:12:42 +0300

    Updates the author field of the RootQueryType to return data from mongoDB

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -82,6 +82,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // return _.find(authors, { id: args.id })
+        return Author.findById(args.id);
       }
     },
     books: {
