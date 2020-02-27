cb34e15460167f7ff997b4a52c91c0fb42dc88c3
Luke Olali <mugapedia@gmail.com>
2020-02-27 10:56:22 +0300

    Renders the BookList component within App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
       <h1>Apollo Client for the GraphQL Server</h1>
+      <BookList />
     </div>
   );
 }
