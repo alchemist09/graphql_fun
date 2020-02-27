fd5ca0d3b47e4cf2433ff91f9cac21b26b6fbf1c
Luke Olali <mugapedia@gmail.com>
2020-02-27 12:01:24 +0300

    Creates an instance of Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
 import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
