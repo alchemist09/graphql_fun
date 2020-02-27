fcf01933d668cdccf5f3bb97024aa8d19d8d8bac
Luke Olali <mugapedia@gmail.com>
2020-02-27 14:43:24 +0300

    Declares the query used to fetch list of books from our graphql server

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      genre
+    }
+  }
+`
 
 class BookList extends Component {
   render() {
