9e9ffaddb293ee2a47ca2ec4c0315c5fc4a0aa0c
Luke Olali <mugapedia@gmail.com>
2020-02-28 10:56:07 +0300

    Declares the AddBook component. Renders it within the App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -13,6 +14,7 @@ function App() {
       <div className="App">
         <h1>Apollo Client for the GraphQL Server</h1>
         <BookList />
+        <AddBook />
       </div>
     </ApolloProvider>
   );
---
+++ client/src/components/AddBook.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class AddBook extends Component {
+
+  render() {
+    console.log(this.props);
+    return (
+      <form>
+        <div>
+          <label htmlFor="name">Book Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="genre">Genre</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="name">Author</label>
+          <select>
+            <option>Select Author</option>
+          </select>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default AddBook;
