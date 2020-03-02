3be0b9b97ab4ba5122cb2f93f5154e28ff175253
Luke Olali <mugapedia@gmail.com>
2020-03-02 16:34:58 +0300

    Binds the the getBookQuery to the BookDetail component

--- client/src/components/BookDetail.js
+++ client/src/components/BookDetail.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { graphql } from '@apollo/react-hoc';
+import { getBookQuery } from '../queries';
 
 class BookDetail extends Component {
   render() {
     return (
       <div id="book-detail">
-      
+        <p>Book Details Goes Here</p>
       </div>
     )
   }
 }
 
-export default BookDetail;
+export default graphql(getBookQuery)(BookDetail);
