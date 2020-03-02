ce836f16ad4a7b239b825f74fd837cf2ca875bac
Luke Olali <mugapedia@gmail.com>
2020-03-02 23:01:25 +0300

    Configures the getBookQuery with the necessary query parameters that will be used to execute it

--- client/src/components/BookDetail.js
+++ client/src/components/BookDetail.js
@@ -4,6 +4,7 @@ import { getBookQuery } from '../queries';
 
 class BookDetail extends Component {
   render() {
+    console.log(this.props);
     return (
       <div id="book-detail">
         <p>Book Details Goes Here</p>
@@ -12,4 +13,10 @@ class BookDetail extends Component {
   }
 }
 
-export default graphql(getBookQuery)(BookDetail);
+export default graphql(getBookQuery, {
+  options: props => ({
+    variables: {
+      id: props.bookID
+    }
+  })
+})(BookDetail);
