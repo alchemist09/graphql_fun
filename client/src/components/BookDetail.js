3a737ab9b4165cf343c7196069510e89e63d0848
Luke Olali <mugapedia@gmail.com>
2020-03-02 16:20:33 +0300

    Declares the BookDetail component

---
+++ client/src/components/BookDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BookDetail extends Component {
+  render() {
+    return (
+      <div id="book-detail">
+      
+      </div>
+    )
+  }
+}
+
+export default BookDetail;
