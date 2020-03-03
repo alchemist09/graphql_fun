5907631acfdd78907f0c2035f0684b9427f524e0
Luke Olali <mugapedia@gmail.com>
2020-03-03 12:17:54 +0300

    Defines method used to display details of selected book

--- client/src/components/BookDetail.js
+++ client/src/components/BookDetail.js
@@ -3,12 +3,40 @@ import { graphql } from '@apollo/react-hoc';
 import { getBookQuery } from '../queries';
 
 class BookDetail extends Component {
+
+  displayBookDetails() {
+    const { book } = this.props.data
+    if(book) {
+      return (
+        <div>
+          <hr />
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.surname}</p>
+          <p>{book.author.titles}</p>
+          <ul>
+            {
+              book.author.books.map(thisBook => {
+                return <li key={thisBook.id}>{thisBook.name}</li>
+              })
+            }
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <hr />
+          <p>Book Details Goes Here</p>
+        </React.Fragment>
+      ); 
+    }
+  }
+
   render() {
     console.log(this.props);
     return (
-      <div id="book-detail">
-        <p>Book Details Goes Here</p>
-      </div>
+      this.displayBookDetails()
     )
   }
 }
